2b839a89f6baa8f11d4051595d68827d952abd7e
Ashitosh27 <rathodashitosh@gmail.com>
2021-12-13 12:52:06 +0530

    Multiply.java commited

---
+++ multiply.java
@@ -0,0 +1,9 @@
+class multiply{
+        public static void main(String args[])
+        {
+                int a=10;
+                int b=5;
+                System.out.println("Multiplication is :"+a*b);
+        }
+}
+
